d9547b6c379501f513c4a6d6a65b0775549ec193
unknown <acemonkey2@gmail.com>
2021-05-23 14:14:40 +0800

    yes

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7a56c15eee8dffdc716af8121622ec22'
 app.config['SQLALCHEMY_TRACK_MODIDFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://azupiyqwdemjrw:305b9ddad54d07868e1626697d5c8ad56d00965f7560bfb43f1457c5913cd06e@ec2-52-22-161-59.compute-1.amazonaws.com:5432/dc0cq7olc0ea15'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://monsywklmjvnbw:9da65ca06e0f8617868ccfb29d4c6e7939dac629227f92f3df9971b4d31c85c7@ec2-18-215-111-67.compute-1.amazonaws.com:5432/dcu6d7n401lbtt'
 
 db = SQLAlchemy(app)
 
