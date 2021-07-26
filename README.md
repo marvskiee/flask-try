eb20519e8600bba0dad6f6cb0410b9867e32040e
unknown <acemonkey2@gmail.com>
2021-07-26 19:46:16 +0800

    Revert "first commit"
    This reverts commit 76430334e87b63ea35982f7ac68a3f4bce880116.

---
+++ README.md
@@ -0,0 +1,7 @@
+# flask-try
+
+To install pipenv type the ff. 
+- pipenv shell
+
+To run the program
+- py run.py
--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,38 @@
 from os import defpath
 
-from flask import render_template,url_for,request
+from flask import render_template,url_for
 from werkzeug.utils import redirect
 from app import app
 from app.models import *
 from app import db
 
-import requests
 import json
 
 @app.route('/')
 def index():
-    return 'home'
+    if Songs.query.count() < 1:
+        with open('phchords.json') as f:
+            data = json.load(f)
+        for d in data:
+            song = Songs(mode=d['mode'], artist=d['artist'], chords=d['chords'], title=d['title'],lyrics=d['lyrics'])    
+            db.session.add(song)
+        db.session.commit()
+        
+    return render_template('index.html')
 
-@app.route('/webhook', methods=['POST','GET'])
-def webhoook():
-    status=mode=token=challenge = ''
-    VERIFY_TOKEN = '7a56c15eee8dffdc716af8121622ec22'
-    if request.method == 'POST':
-        if request.get_json().get('object')=='page':
-            entry = request.get_json().get('entry')
-            for e in entry:
-                print(e['messaging'][0])
-            return 'EVENT_RECEIVED'
-        else:
-            return '404'
+@app.route('/search/<string:song>',methods=['POST','GET'])
+def song(song):
+    result = Songs.query.filter(Songs.title.like(f'%{song}%')).all()
+    return render_template('search.html',result=result)
+@app.route('/request')
+def show():
+    result = Request.query.all()
+    return render_template('request.html',result=result)
 
-
-    if 'hub.mode' in request.args:
-        mode = request.args.get('hub.mode')
-        print(mode)
-    if 'hub.verify_token' in request.args:
-        token = request.args.get('hub.verify_token')
-        print(token)
-    if 'hub.challenge' in request.args:
-        challenge = request.args.get('hub.challenges')
-        print(challenge)
-    
-    if mode and token:
-        if mode == 'subscribe' and token == VERIFY_TOKEN:
-            return 'WEBHOOK VERIFIED'
-    else:
-        return 'TOKENS DO NOT MATCH'
+@app.route('/request/<string:data>/<string:status>',methods=['POST','GET'])
+def request(data,status):
+    req = Request(data=data,status=status)
+    db.session.add(req)
+    db.session.commit()
+    result = Request.query.all()
+    return redirect('/request')
